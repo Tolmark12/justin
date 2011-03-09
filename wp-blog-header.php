442d64b51d49d95f745503fc88939eee857e6a77
Mark Parson <mark@delorum.com>
2011-03-09 13:29:50 -0700

    test

--- wp-blog-header.php
+++ wp-blog-header.php
@@ -7,6 +7,7 @@
 
 if ( !isset($wp_did_header) ) {
 
+	echo("here");
 	$wp_did_header = true;
 
 	require_once( dirname(__FILE__) . '/wp-load.php' );
@@ -15,6 +16,8 @@ if ( !isset($wp_did_header) ) {
 
 	require_once( ABSPATH . WPINC . '/template-loader.php' );
 
+}else{
+	echo "else";
 }
 
 ?>
