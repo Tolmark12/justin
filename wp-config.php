12cc4499e3eb2a3dc2da62c3969eab0a1df7ca5f
Mark Parson <mark@delorum.com>
2011-03-09 13:35:39 -0700

    refresh

--- wp-config.php
+++ wp-config.php
@@ -16,16 +16,16 @@
 
 // ** MySQL settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define('DB_NAME', 'database_name_here');
+define('DB_NAME', 'dina');
 
 /** MySQL database username */
-define('DB_USER', 'username_here');
+define('DB_USER', 'paula');
 
 /** MySQL database password */
-define('DB_PASSWORD', 'password_here');
+define('DB_PASSWORD', '9B81oYOw');
 
 /** MySQL hostname */
-define('DB_HOST', 'localhost');
+define('DB_HOST', 'localhost:/tmp/mysql/dina.sock');
 
 /** Database Charset to use in creating database tables. */
 define('DB_CHARSET', 'utf8');
