0daf7df7c5009e7f6f31f9c1c177d08bbb638d4a
Mark Parson <mark@delorum.com>
2011-04-16 22:49:20 -0600

    asdf

--- wp-content/themes/Glider/header.php
+++ wp-content/themes/Glider/header.php
@@ -10,7 +10,7 @@
 <script type="text/javascript" src="http://use.typekit.com/voz3nft.js"></script>
 <script type="text/javascript">try{Typekit.load();}catch(e){}</script>
 
-
+<link rel="stylesheet" href="http://justin.pagodabox.com/wp-content/themes/Glider/player-style.css" type="text/css" media="screen" title="no title" charset="utf-8">	
 <link rel="stylesheet" href="<?php bloginfo('stylesheet_url'); ?>" type="text/css" media="screen" />
 <link rel="stylesheet" href="<?php bloginfo('template_directory'); ?>/css/jquery.fancybox-1.2.6.css" type="text/css" media="screen" />
 <link rel="alternate" type="application/rss+xml" title="<?php bloginfo('name'); ?> RSS Feed" href="<?php bloginfo('rss2_url'); ?>" />
@@ -30,6 +30,214 @@
 <script type="text/javascript">
    document.documentElement.className = 'js';
 </script>
+
+	
+	<script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.4.1/jquery.min.js"></script>
+	<script type="text/javascript" src="http://justin.pagodabox.com/wp-content/themes/Glider/js/jquery.jplayer.min.js"></script>
+	<script type="text/javascript">
+	//<![CDATA[
+
+	$(document).ready(function(){
+alert( $ );
+		var Playlist = function(instance, playlist, options) {
+			var self = this;
+
+			this.instance = instance; // String: To associate specific HTML with this playlist
+			this.playlist = playlist; // Array of Objects: The playlist
+			this.options = options; // Object: The jPlayer constructor options for this playlist
+
+			this.current = 0;
+
+			this.cssId = {
+				jPlayer: "jquery_jplayer_",
+				interface: "jp_interface_",
+				playlist: "jp_playlist_"
+			};
+			this.cssSelector = {};
+
+			$.each(this.cssId, function(entity, id) {
+				self.cssSelector[entity] = "#" + id + self.instance;
+			});
+
+			if(!this.options.cssSelectorAncestor) {
+				this.options.cssSelectorAncestor = this.cssSelector.interface;
+			}
+
+			$(this.cssSelector.jPlayer).jPlayer(this.options);
+
+			$(this.cssSelector.interface + " .jp-previous").click(function() {
+				self.playlistPrev();
+				$(this).blur();
+				return false;
+			});
+
+			$(this.cssSelector.interface + " .jp-next").click(function() {
+				self.playlistNext();
+				$(this).blur();
+				return false;
+			});
+		};
+
+		Playlist.prototype = {
+			displayPlaylist: function() {
+				var self = this;
+				$(this.cssSelector.playlist + " ul").empty();
+				for (i=0; i < this.playlist.length; i++) {
+					var listItem = (i === this.playlist.length-1) ? "<li class='jp-playlist-last'>" : "<li>";
+					listItem += "<a href='#' id='" + this.cssId.playlist + this.instance + "_item_" + i +"' tabindex='1'>"+ this.playlist[i].name +"</a>";
+
+					// Create links to free media
+					if(this.playlist[i].free) {
+						var first = true;
+						listItem += "<div class='jp-free-media'>(";
+						$.each(this.playlist[i], function(property,value) {
+							if($.jPlayer.prototype.format[property]) { // Check property is a media format.
+								if(first) {
+									first = false;
+								} else {
+									listItem += " | ";
+								}
+								listItem += "<a id='" + self.cssId.playlist + self.instance + "_item_" + i + "_" + property + "' href='" + value + "' tabindex='1'>" + property + "</a>";
+							}
+						});
+						listItem += ")</span>";
+					}
+
+					listItem += "</li>";
+
+					// Associate playlist items with their media
+					$(this.cssSelector.playlist + " ul").append(listItem);
+					$(this.cssSelector.playlist + "_item_" + i).data("index", i).click(function() {
+						var index = $(this).data("index");
+						if(self.current !== index) {
+							self.playlistChange(index);
+						} else {
+							$(self.cssSelector.jPlayer).jPlayer("play");
+						}
+						$(this).blur();
+						return false;
+					});
+
+					// Disable free media links to force access via right click
+					if(this.playlist[i].free) {
+						$.each(this.playlist[i], function(property,value) {
+							if($.jPlayer.prototype.format[property]) { // Check property is a media format.
+								$(self.cssSelector.playlist + "_item_" + i + "_" + property).data("index", i).click(function() {
+									var index = $(this).data("index");
+									$(self.cssSelector.playlist + "_item_" + index).click();
+									$(this).blur();
+									return false;
+								});
+							}
+						});
+					}
+				}
+			},
+			playlistInit: function(autoplay) {
+				if(autoplay) {
+					this.playlistChange(this.current);
+				} else {
+					this.playlistConfig(this.current);
+				}
+			},
+			playlistConfig: function(index) {
+				$(this.cssSelector.playlist + "_item_" + this.current).removeClass("jp-playlist-current").parent().removeClass("jp-playlist-current");
+				$(this.cssSelector.playlist + "_item_" + index).addClass("jp-playlist-current").parent().addClass("jp-playlist-current");
+				this.current = index;
+				$(this.cssSelector.jPlayer).jPlayer("setMedia", this.playlist[this.current]);
+			},
+			playlistChange: function(index) {
+				this.playlistConfig(index);
+				$(this.cssSelector.jPlayer).jPlayer("play");
+			},
+			playlistNext: function() {
+				var index = (this.current + 1 < this.playlist.length) ? this.current + 1 : 0;
+				this.playlistChange(index);
+			},
+			playlistPrev: function() {
+				var index = (this.current - 1 >= 0) ? this.current - 1 : this.playlist.length - 1;
+				this.playlistChange(index);
+			}
+		};
+
+		var mediaPlaylist = new Playlist("1", [
+			// {
+			// 	name:"Big Buck Bunny Trailer",
+			// 	m4v:"http://www.jplayer.org/video/m4v/Big_Buck_Bunny_Trailer_480x270_h264aac.m4v",
+			// 	
+			// 	poster:"http://www.jplayer.org/video/poster/Big_Buck_Bunny_Trailer_480x270.png"
+			// },
+			{
+				name:"Classical Guitar Demo",
+				mp3:"http://justin.pagodabox.com/wp-content/uploads/2011/04/Classical-Guitar-Mp3.mp3",
+				poster: "http://justin.pagodabox.com/wp-content/uploads/2011/04/A-Wedding-Ceremony_Classical-Demo-Image.jpg"
+			},
+			{
+				name:"Vocals Demo",
+				mp3:"http://justin.pagodabox.com/wp-content/uploads/2011/04/Vocal-Demo.mp3",
+				poster: "http://justin.pagodabox.com/wp-content/uploads/2011/04/B-Guitar_Vocal-Demo-Image.jpg"
+			},
+			{
+				name:"Party Band Demo",
+				mp3:"http://justin.pagodabox.com/wp-content/uploads/2011/04/C-Party-Band-Demo.mp3",
+				poster: "http://justin.pagodabox.com/wp-content/uploads/2011/04/C-Party-Band-Demo-Image.jpg"
+			},
+			{
+				name:"Spanish Guitar Demo",
+				mp3:"http://justin.pagodabox.com/wp-content/uploads/2011/04/D-Spanish-Guitar-Demo.mp3",
+				poster: "http://justin.pagodabox.com/wp-content/uploads/2011/04/D-Spanish-Guitar-Demo-Image.jpg"
+			},
+			{
+				name:"Solo Guitar Demo",
+				mp3:"http://justin.pagodabox.com/wp-content/uploads/2011/04/E-Solo-Guitar-Demo-New.mp3",
+				poster: "http://justin.pagodabox.com/wp-content/uploads/2011/04/E-Solo-Guitar-Demo-Image.jpg"
+			},
+			{
+				name:"Jazz Trio Demo",
+				m4v: "http://justin.pagodabox.com/wp-content/uploads/2011/04/F-Jazz-Trio-Demo.mp3",
+				poster: "http://justin.pagodabox.com/wp-content/uploads/2011/04/F-Jazz-Trio-Demo-Image.jpg"
+			},
+			{
+				name:"Gypsy Jazz Demo",
+				mp3:"http://justin.pagodabox.com/wp-content/uploads/2011/04/G-Gypsy-Jazz-Group-Demo.mp3",
+				poster: "http://justin.pagodabox.com/wp-content/uploads/2011/04/G-Gypsy-Jazz-Trio-Demo-Image.jpg"
+			},
+			{
+				name:"World Music Demo",
+				mp3: "http://justin.pagodabox.com/wp-content/uploads/2011/04/Ethnic-Demo.mp3",
+				poster: "http://justin.pagodabox.com/wp-content/uploads/2011/04/H-World_Ethnic-Demo-Image.jpg"
+			},
+			{
+				name:"Accoustic Guitar Demo",
+				mp3:"http://justin.pagodabox.com/wp-content/uploads/2011/04/I-Acoustic-Guitar-Duo-Demo.mp3",
+				poster: "http://justin.pagodabox.com/wp-content/uploads/2011/04/I-Acoustic-Guitar-Duo-Demo-Image.jpg"
+			},
+
+
+
+
+
+
+
+		], {
+			ready: function() {
+				mediaPlaylist.displayPlaylist();
+				mediaPlaylist.playlistInit(false); // Parameter is a boolean for autoplay.
+			},
+			ended: function() {
+				mediaPlaylist.playlistNext();
+			},
+			swfPath: "js",
+			solution: "flash, html",
+			supplied: "ogv, m4v, oga, mp3"
+		});
+
+		// $("#jplayer_inspector_1").jPlayerInspector({jPlayer:$("#jquery_jplayer_1")});
+	});
+	//]]>
+	alert("first...");
+
+	</script>
 
 <?php if ( is_singular() ) wp_enqueue_script( 'comment-reply' ); ?>
 <?php wp_head(); ?>
