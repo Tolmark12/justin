ec56ca47a45548bb78e5bc02869aab8655f85db6
Jed Galbraith <jed@delorum.com>
2011-10-31 12:36:53 -0600

    echo before db cred

--- wp-config.php
+++ wp-config.php
@@ -33,6 +33,8 @@ define('DB_CHARSET', 'utf8');
 // The Database Collate type. Don't change this if in doubt. 
 define('DB_COLLATE', '');
 //*/
+echo "before db cred";
+exit;
 
 if (isset($_SERVER['PAGODA']) && $_SERVER['PAGODA'] == 'TRUE') {
   echo "using pagoda cred";
