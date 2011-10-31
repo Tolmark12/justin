393580137dc8126a5e8e165e67b7ecde6b1b8e39
Mark Parson <mark@delorum.com>
2011-10-31 12:22:35 -0600

    fixed error

--- wp-config.php
+++ wp-config.php
@@ -36,7 +36,7 @@ define('DB_COLLATE', '');
 
 define('DB_NAME', $_SERVER['DB_NAME']);
 define('DB_USER', $_SERVER['DB_USER']);
-define('DB_PASS', $_SERVER['DB_PASSWORD']);
+define('DB_PASSWORD', $_SERVER['DB_PASS']);
 define('DB_HOST', "tunnel.newpagodabox.com:3306");
 
 
